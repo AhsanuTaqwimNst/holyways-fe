2a6f8799ef4fbd551042760dd15ab2e1c2d22b00
ahsanutaqwimns <ahsanu030721@gmail.com>
2023-02-13 13:26:01 +0700

    benar

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL:"holyways-be-production.up.railway.app/api/v1/"
+  baseURL:"https://holyways-be-production-83fc.up.railway.app//api/v1/"
 
 });
 
